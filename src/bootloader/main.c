488e9ded7a9835030fd475934b7a64d39b7d38f5
Thijs Kruithof <39615244+thijskruithof@users.noreply.github.com>
2024-12-08 15:42:26 +0100

    Adding some asserts and error checks to find out why the bootloader is failing

--- src/bootloader/main.c
+++ src/bootloader/main.c
@@ -1,25 +1,24 @@
-// UEFI bare bones example, from https://wiki.osdev.org/UEFI_App_Bare_Bones#hello.c
-
-
 #include <efi.h>
 #include <efilib.h>
 #include <elf.h>
 #include <lib.h>
 
 
+#define VERIFY(status) \
+	if (EFI_ERROR(status)) TerminateOnError(__FUNCTION__ ## " at line " ## __LINE__);
+
+#define ASSERT(cond) \
+	if (!(cond)) TerminateOnError("Assert in " ## __FUNCTION__ ## " at line " ## __LINE__);
 
 /**
 @brief Verify the provided status, and if it is an error, wait for a keystroke before returning.
 @return FALSE on error, otherwise TRUE
 **/
-BOOLEAN VerifyStatus(EFI_STATUS inStatus)
+void TerminateOnError(const char* inError)
 {
-    if (!EFI_ERROR(inStatus))
-        return TRUE;
+	EFI_STATUS con_status;
 
-    EFI_STATUS con_status = gST->ConOut->OutputString(gST->ConOut, L"Error detected while initializing Tos. Press any key to continue.");
-    if (EFI_ERROR(con_status))
-        return FALSE;
+    Print(L"Error while initializing Tos: %a\n\r", inError);
 
     // Wait for a keystroke before continuing, otherwise your
     // message will flash off the screen before you see it.
@@ -43,45 +42,32 @@ BOOLEAN VerifyStatus(EFI_STATUS inStatus)
 /** 
 @brief Open a file on disk
 **/
-EFI_STATUS OpenFile(CHAR16* inFilename, EFI_FILE** outFile)
+EFI_FILE* OpenFile(CHAR16* inFilename)
 {
 	EFI_STATUS status;
 
 	// Get the Loaded Image protocol
 	EFI_LOADED_IMAGE_PROTOCOL* loaded_image = NULL;
-	status = gST->BootServices->HandleProtocol(LibImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&loaded_image);
-	if (EFI_ERROR(status))
-		return status;
-	if (loaded_image == NULL)
-		return EFI_LOAD_ERROR;		
+	VERIFY(gST->BootServices->HandleProtocol(LibImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&loaded_image));
+	ASSERT(loaded_image != NULL);	
 
 	// Get its File System protocol
 	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* filesystem = NULL;
-	status = gST->BootServices->HandleProtocol(loaded_image->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)&filesystem);
-	if (EFI_ERROR(status))
-		return status;
-	if (filesystem == NULL)
-		return EFI_LOAD_ERROR;		
+	VERIFY(gST->BootServices->HandleProtocol(loaded_image->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)&filesystem));
+	ASSERT(filesystem != NULL);		
 
 	// Open our File System volume (in the root directory)
 	EFI_FILE* directory = NULL;
-	status = filesystem->OpenVolume(filesystem, &directory);
-	if (EFI_ERROR(status))
-		return status;
-	if (directory == NULL)
-		return EFI_LOAD_ERROR;		
+	VERIFY(filesystem->OpenVolume(filesystem, &directory));
+	ASSERT(directory != NULL);		
 
 	// Open the specified file by its inFilename
 	EFI_FILE* file = NULL;
-	status = directory->Open(directory, &file, inFilename, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
-	if (EFI_ERROR(status))
-		return status;	
-	if (file == NULL)
-		return EFI_LOAD_ERROR;
+	VERIFY(directory->Open(directory, &file, inFilename, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY));
+	ASSERT(file != NULL);
 
 	// Success!
-	*outFile = file;
-	return EFI_SUCCESS;
+	return file;
 }
 
 
@@ -89,10 +75,8 @@ EFI_STATUS OpenFile(CHAR16* inFilename, EFI_FILE** outFile)
 /**
 @brief Load our ELF from an opened file into memory and return its entry point
 **/
-EFI_STATUS LoadELFFromFile(EFI_FILE* inFile, void** outEntry)
+void* LoadELFFromFile(EFI_FILE* inFile)
 {
-	EFI_STATUS status;
-
 	// // First determine the size of our file info struct
 	// UINTN fileinfo_size = 0;
 	// status = inFile->GetInfo(inFile, &gEfiFileInfoGuid, &fileinfo_size, NULL);
@@ -119,50 +103,37 @@ EFI_STATUS LoadELFFromFile(EFI_FILE* inFile, void** outEntry)
 	// Read our ELF header
 	Elf64_Ehdr hdr;	
 	UINTN hdr_size = sizeof(hdr);
-	status = inFile->Read(inFile, &hdr_size, &hdr);
-	if (!VerifyStatus(status))
-		return status;	
-	if (hdr_size == 0)
-		return EFI_LOAD_ERROR;
+	VERIFY(inFile->Read(inFile, &hdr_size, &hdr));
+	ASSERT(hdr_size > 0);
 
 	// Check first 4 characters (\177ELF)
-	if (hdr.e_ident[EI_MAG0] != ELFMAG0 || hdr.e_ident[EI_MAG1] != ELFMAG1 ||
-		hdr.e_ident[EI_MAG2] != ELFMAG2 || hdr.e_ident[EI_MAG3] != ELFMAG3)
-	{
-		return EFI_LOAD_ERROR;
-	}
+	ASSERT(hdr.e_ident[EI_MAG0] == ELFMAG0);
+	ASSERT(hdr.e_ident[EI_MAG1] == ELFMAG1);
+	ASSERT(hdr.e_ident[EI_MAG2] == ELFMAG2);
+	ASSERT(hdr.e_ident[EI_MAG3] == ELFMAG3);
 
 	// 64 bit object?
 	// Executable?
 	// 2's complement, little endian?
 	// 64bit x86?
 	// Currently support elf format version?
-	if (hdr.e_ident[EI_CLASS] != ELFCLASS64 ||
-		hdr.e_type != ET_EXEC ||
-		hdr.e_ident[EI_DATA] != ELFDATA2LSB ||
-		hdr.e_machine != EM_X86_64 ||
-		hdr.e_version != EV_CURRENT)
-	{
-		return EFI_LOAD_ERROR;
-	}
+	ASSERT(hdr.e_ident[EI_CLASS] == ELFCLASS64);
+	ASSERT(hdr.e_type == ET_EXEC);
+	ASSERT(hdr.e_ident[EI_DATA] == ELFDATA2LSB);
+	ASSERT(hdr.e_machine == EM_X86_64);
+	ASSERT(hdr.e_version == EV_CURRENT);
 
 	Elf64_Phdr* phdrs;
 	
 	// Seek to the page headers offset in the file
-	status = inFile->SetPosition(inFile, hdr.e_phoff);
-	if (!VerifyStatus(status))
-		return status;		
+	VERIFY(inFile->SetPosition(inFile, hdr.e_phoff));
 
 	// Calculate the size of the page headers
 	UINTN phdrs_size = hdr.e_phnum * hdr.e_phentsize;
-	status = gST->BootServices->AllocatePool(EfiLoaderData, phdrs_size, (void**)&phdrs);
-	if (!VerifyStatus(status))
-		return status;
+	VERIFY(gST->BootServices->AllocatePool(EfiLoaderData, phdrs_size, (void**)&phdrs));
 
 	// Read all the page headers
-	status = inFile->Read(inFile, &phdrs_size, phdrs);
-	if (!VerifyStatus(status))
-		return status;
+	VERIFY(inFile->Read(inFile, &phdrs_size, phdrs));
 
 	// Read all relevant page headers
 	char* phdr8 = (char*)phdrs;
@@ -179,30 +150,22 @@ EFI_STATUS LoadELFFromFile(EFI_FILE* inFile, void** outEntry)
 
 			// Allocate memory pages at the preferred address
 			Elf64_Addr mem_addr = phdr->p_paddr;
-			status = gST->BootServices->AllocatePages(AllocateAddress, EfiLoaderData, num_mem_pages, &mem_addr);
-			if (!VerifyStatus(status))
-				return status;
+			VERIFY(gST->BootServices->AllocatePages(AllocateAddress, EfiLoaderData, num_mem_pages, &mem_addr));
 
 			// Seek to the start offset in the file
-			status = inFile->SetPosition(inFile, phdr->p_offset);
-			if (!VerifyStatus(status))
-				return status;
+			VERIFY(inFile->SetPosition(inFile, phdr->p_offset));
 
 			// Read the data from disk
 			UINTN disk_size = phdr->p_filesz;
-			status = inFile->Read(inFile, &disk_size, (void*)mem_addr);
-			if (!VerifyStatus(status))
-				return status;
+			VERIFY(inFile->Read(inFile, &disk_size, (void*)mem_addr));
 		}		
 
 		// Move our ptr forward
 		phdr8 += hdr.e_phentsize;
 	}
 
-	// Store the entry point address
-	*outEntry = (void*)hdr.e_entry;
-
-    return EFI_SUCCESS;
+	// Return the entry point address
+	return (void*)hdr.e_entry;
 }
 
 
@@ -210,26 +173,15 @@ EFI_STATUS LoadELFFromFile(EFI_FILE* inFile, void** outEntry)
 /**
 @brief Load an ELF from a file with the given name and return its entry point
 **/
-EFI_STATUS LoadELF(CHAR16* inPath, void** outEntry)
+void* LoadELF(CHAR16* inPath)
 {
 	EFI_STATUS status;
-	EFI_FILE* file = NULL;
 	
 	// Load the file to memory
-	status = OpenFile(inPath, &file);
-	if (EFI_ERROR(status))
-		return status;
-	if (file == NULL)
-		return EFI_LOAD_ERROR;
+	EFI_FILE* file = OpenFile(inPath);
 
     // Load the ELF into memory and get the entry point address
-    void* entry = NULL;
-	status = LoadELFFromFile(file, &entry);
-    if (EFI_ERROR(status))
-        return status;
-
-    *outEntry = entry;
-	return EFI_SUCCESS;
+    return LoadELFFromFile(file);
 }
 
 
